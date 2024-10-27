1bd6da9402fd5aab0d286304aba1e10924cdb8d8
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 03:05:06 +0200

    Добавьте файлы проекта.

---
+++ Plus-one/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0; // Set the current digit to 0 and continue the carry-over
+        }
+        // If all digits were 9, we need an additional digit at the start
+        int[] result = new int[digits.Length + 1];
+        result[0] = 1; // Place 1 at the beginning
+        return result;
+    }
+}
